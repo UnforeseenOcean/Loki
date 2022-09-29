8781d845fe46bdaa26a09e9dad42c76e5a7b9c20
Sean Canned Bean <UnforeseenOcean@users.noreply.github.com>
2022-09-30 01:03:29 +0900

    Code now ignores HW status
    Hence, the A0 pin is unused. All the bad practice codes were removed and partially rewritten.
    If you already built Loki, this code should still work. This also fixes the unresponsive button issue on modules using `MH2924K-24SS` MP3/WAV player ASIC.

--- Loki/Loki.ino
+++ Loki/Loki.ino
@@ -1,5 +1,5 @@
 /*
- * Loki (Revision 1006)
+ * Loki (Revision 1441)
  * (C) 2022 Blackbeard Softworks
  * 
  * This code relies on external hardware to be present.
@@ -52,6 +52,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println(F("Loki v1.1 starting up..."));
+  delay(2000);
   if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
@@ -61,7 +62,7 @@ void setup() {
     }
   }
   Serial.println(F("DFPlayer Mini online."));
-  myDFPlayer.setTimeOut(1000);
+  myDFPlayer.setTimeOut(1500);
   myDFPlayer.volume(30);  //Set volume value. From 0 to 30
   myDFPlayer.playMp3Folder(1);  //Play the first mp3
   delay(250);
@@ -69,7 +70,7 @@ void setup() {
     // Wait while the track finishes playing
   }
   EIFR = 1;
-  delay(8000);
+  delay(7000);
   attachInterrupt(digitalPinToInterrupt(button), playbackCtrl, FALLING);
   Serial.println("Ready");
 }
@@ -88,8 +89,8 @@ void loop() {
     Serial.println(" file");
     myDFPlayer.playMp3Folder(songIndex);
     delay(50);
-    while (digitalRead(statusPin) == LOW) {
-      Serial.println("playing ");
+    while (stopped == false) {
+      Serial.println("playing");
     }
     state = false;
   }
@@ -101,7 +102,9 @@ void loop() {
       stopped = true;
     }
   }
-
+  if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }
 }
 
 void playbackCtrl() {
@@ -109,12 +112,12 @@ void playbackCtrl() {
   unsigned long inttime = millis();
   if (inttime - lit > 1500) {
     if (state == true) {
-      myDFPlayer.stop();
       state = false;
+      stopped = true;
     }
     if (state == false) {
-      myDFPlayer.stop();
       state = true;
+      stopped = true;
     }
     songIndex++;
     if (songIndex > maxSongCount || songIndex < 2) {
@@ -122,4 +125,66 @@ void playbackCtrl() {
     }
   }
   lit = inttime;
+}
+
+void printDetail(uint8_t type, int value) {
+
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
 }
